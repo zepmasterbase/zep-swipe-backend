c6ef19937fbd2734a8f46e70be41ccc798fb1bab
zepmasterbase <masterzep04@gmail.com>
2026-07-06 12:42:31 +0200

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -3,12 +3,5 @@ const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBh
 
 const db = supabase.createClient(
   SUPABASE_URL,
-  SUPABASE_ANON_KEY,
-  {
-    auth: {
-      persistSession: true,
-      autoRefreshToken: true,
-      detectSessionInUrl: true
-    }
-  }
+  SUPABASE_ANON_KEY
 );
