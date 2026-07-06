9ddb09afdea0a0a5711f12e915c48dcd6cfd3fe1
zepmasterbase <masterzep04@gmail.com>
2026-07-06 12:08:04 +0200

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,4 +1,4 @@
-const SUPABASE_URL = "PASTE_YOUR_PROJECT_URL_HERE";
+const SUPABASE_URL = "https://tieekcelcathtuluwcfc.supabase.co";
 const SUPABASE_ANON_KEY = "PASTE_YOUR_ANON_PUBLIC_KEY_HERE";
 
 const db = supabase.createClient(
